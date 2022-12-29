60c820a56c2395c3a3b88107e3323983e51e45b5
giimiks <fakinafakuka@gmail.com>
2022-12-29 10:56:38 +0100

    Basic příprava game enginu

---
+++ gamesa/assets.py
@@ -0,0 +1,23 @@
+map = {
+      "Karla Čapka": {
+            "desc": "Ááá, ulice Karla Čapka... Nejpodivuhodnější nátury z celého Habartova se v místních bytech scházejí k různým jistě legálním aktivitám.",
+            "items": {},
+            "zkoumej":  {"Nedopalek", "Kamínek", "Zapalovač"},
+            "where": {"Raisovka", "Skatepark", "Texas"},
+            "obstacles": {}
+            },
+      "Raisovka": {
+            "desc": "Raisova ulice - protáhlá ulička s paneláčky, které mají přívětivě barevnou fasádu. Škoda, že stejně přívětiví nejsou místní obyvatelé...",
+            "items": {},
+            "zkoumej": {"Klacek", "Květ", "Banán"},
+            "where": {"Karla Čapka"},
+            "obstacles": {"Hladové dítě"}
+      },
+
+}
+items = {}
+obstacles = {}
+inventory = {"Igelitka", "Telefon", "Peníze"}
+quotes = ("Co dál?", "Kam se vydáš?", "Další krok?", "Co hodláš dál dělat?", "Jak budeš pokračovat?", "Jak budeš postupovat?", "Co se bude dít dál?", "Co dál uděláš?")
+
+
---
+++ gamesa/main.py
@@ -0,0 +1,43 @@
+#do 29.12.2022 - 2,5h ztraceno
+#29.12.2022 - 1h session
+
+import assets as currentState
+from random import randint
+from time import sleep
+
+def typeOut(text: str, speed: int) -> None:
+    i = 0
+    x = str('')
+    for char in text:
+        x+=char
+        i+=1
+        sleep(speed)
+        print(char, end='', flush=True)
+        if i % 96 == 0 and char == ' ':
+            print()
+def randomQuote(text: tuple[str]) -> str:
+   return text[randint(0, len(text)-1)] 
+
+def jdi(args, pos):
+    args.pop(0)
+    args = ' '.join(args)
+    if args in pos.get('where'):
+        return currentState.map.get(args)
+    else: return pos
+
+commands = {"jdi": jdi, "seber":{}, "polož":{}, "zkoumej":{}, "použij":{}, "save":{}, "load":{}, "info":{}, "help": {}}
+
+position = currentState.map.get("Karla Čapka")
+inventory = currentState.inventory
+
+while True:
+    print(position)
+    prompt = input(typeOut(str(randomQuote(currentState.quotes) + ' > '), 0.05)).split(' ')
+    if prompt[0] in commands:
+        print('true')
+        position = commands.get(prompt[0])(prompt, position)
+        print(position)
+            
+
+
+

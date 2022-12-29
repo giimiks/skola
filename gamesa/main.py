5165c55ef7697a39ce35ad904652ca0b9811b668
giimiks <fakinafakuka@gmail.com>
2022-12-29 18:32:39 +0100

    úprava komentáře

--- gamesa/main.py
+++ gamesa/main.py
@@ -1,6 +1,6 @@
 #do 29.12.2022 - 2,5h ztraceno
 #28.12.2022 - 1h session
-#29.12.2022 - 4h session (zatím)
+#29.12.2022 - 7,75h session 
 
 from assets import *
 from commands import commands, odcesti, typeOut, randomQuote, arguments, cmdOutput
